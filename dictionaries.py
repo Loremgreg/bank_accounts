d098e47b7fe2e419c6f16289fd25ef2f12afb89e
Loremgreg <gregorybastin.de@gmail.com>
2025-07-20 19:46:28 +0200

    diction

---
+++ dictionaries.py
@@ -0,0 +1,52 @@
+band = {"vocals": "Plant", "guitar": "Page"}
+
+band2 = dict(vocals="Plant", guitar="Page")
+
+
+print(band)
+print(band2)
+print(type(band))
+print(len(band))
+
+# Access items
+print(band["vocals"])
+print(band["guitar"])
+
+# list all keys
+for keys in band.keys():
+    print(keys)
+
+# list all values
+for values in band.values():
+    print(values)
+
+# list of key/value pairs as tuples
+for item in band.items():
+    print(item)
+
+# Change values
+band["vocals"] = "Coverdale"
+band.update({"bass": "JPJ"})
+print(band)
+
+# Remove items
+print(band.pop("bass"))
+print(band)
+
+
+band["drums"] = "Bonham"
+print(band)
+
+print(band.popitem())  # tuple
+print(band)
+
+for item in band.items():
+    print(item)
+
+del band["drums"]
+print(band)
+
+band.clear()
+print(band)
+
+
