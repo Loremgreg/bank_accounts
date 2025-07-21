17b6cce64973c1abf5a019010454020a8300235c
Loremgreg <gregorybastin.de@gmail.com>
2025-07-21 13:39:06 +0200

    loops

---
+++ loops.py
@@ -0,0 +1,64 @@
+# value = 1
+# while value <= 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to " + str(value))
+
+# names = ["Dave", "Sara", "John"]
+# for name in names:
+#     print(name)
+
+# for name in "Cannada":
+#     print(name)
+
+# for name in names:
+#     if name == "Sara":
+#         break
+#     print(name)
+
+# for name in names:
+#     if name == "Sara":
+#         continue
+#     print(name)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(2,4):
+#     print(x)
+
+# for x in range(0,100,5):
+#     print(x)
+# else:
+#     print("Glad that\'s over")
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes","eats","sleeps"]
+attrib = ["good", "big", "again"]
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".") 
+
+# for action in actions:
+#     for name in names:
+#         print(name + " " + action + ".") 
+
+# for name in names:
+#     for action in actions:
+#         for attri in attrib:
+#             print(name + " " + action + " " + attri + ".") 
+
+# for action in actions:
+#     for name in names:
+#         for attri in attrib:
+#             print(name + " " + action + " " + attri + ".") 
